0fbb89e1e8c0d6b16434031b4025ae4d58d08d1c
diegobarros <diegobarrosflf@gmail.com>
2021-06-09 11:39:58 -0300

    Method Reference

--- src/OrdenaStrings.java
+++ src/OrdenaStrings.java
@@ -13,9 +13,14 @@ public class OrdenaStrings {
 		palavras.add("Bom dia");
 		palavras.add("Obrigado");
 			
-		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+		//palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+		//palavras.sort(Comparator.comparing(s -> s.length()));
+		//palavras.sort(Comparator.comparing(String::length));
+		palavras.sort(String.CASE_INSENSITIVE_ORDER);
+		
 
-		palavras.forEach(t -> System.out.println(t));
+		//palavras.forEach(t -> System.out.println(t));
+		palavras.forEach(System.out::println);
 
 	}
 
