bcf4d50427ae8276ca5313c530e6e260d605fcd0
diegobarros <diegobarrosflf@gmail.com>
2021-06-09 13:47:43 -0300

    Mais Streams, Collectors e APIs

--- src/CursoTeste.java
+++ src/CursoTeste.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.OptionalDouble;
+import java.util.stream.Collectors;
 
 public class CursoTeste {
 
@@ -16,18 +19,23 @@ public class CursoTeste {
 		
 //		cursos.forEach(c -> System.out.println(c.getNome()));
 		
+		List<Curso> resultado = cursos.stream()
+			.filter(c -> c.getAlunos() > 100)
+			.collect(Collectors.toList());
+		
 		cursos.stream()
-			.filter(c -> c.getAlunos() > 50)
-			.map(Curso::getAlunos)
-			.forEach(System.out::println); 
+				.filter(c -> c.getAlunos() > 100)
+				.collect(Collectors.toMap(
+						c -> c.getNome(),
+						c -> c.getAlunos()))
+				.forEach((nome, alunos) -> System.out.println(nome + "tem " + alunos + " alunos")); 		
 		
-		int sum = cursos.stream()
-			.filter(c -> c.getAlunos() >= 100)
-			.mapToInt(Curso::getAlunos)
-			.sum();
 		
-		System.out.println(sum);
-
+		OptionalDouble media = cursos.stream()
+			.mapToInt(c -> c.getAlunos())
+			.average();
+		
+		System.out.println(media.getAsDouble());
 	}
 
 }
